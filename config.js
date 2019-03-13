53f94319300f6196a3d851dc7087ee1dbf64ad39
k1cho <aadnanhadzipasic@gmail.com>
2019-03-13 10:57:39 +0100

    server setup and mongoose connection

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    database: 'mongodb+srv://k1cho:42yFeFTFXNyIcovq@cluster0-rfzxf.mongodb.net/shop?retryWrites=true',
+    port: 3000,
+    secret: 'k1cho13032019'
+}
---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const config = require('./config')
+const cors = require('cors')
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+app.use(morgan('dev'))
+app.use(cors())
+
+// mongo connection
+mongoose.Promise = global.Promise
+mongoose.connect(config.database, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log('Connected to database');
+}).catch(() => {
+    console.log('Connection error');
+})
+mongoose.set('useCreateIndex', true);
+
+app.listen(config.port, (err) => {
+    console.log('Listening on port ' + config.port);
+})
