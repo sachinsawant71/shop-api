2b6fdc3ba007e526fd856dce6b59b3b200c006c9
k1cho <aadnanhadzipasic@gmail.com>
2019-03-13 16:40:49 +0100

    user model

---
+++ models/user.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const bcrypt = require('bcrypt-nodejs')
+const crypto = require('crypto')
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        unique: true,
+        lowercase: true
+    },
+    name: String,
+    password: String,
+    picture: String,
+    isSeller: {
+        type: Boolean,
+        default: false
+    },
+    address: {
+        addr1: String,
+        add2: String,
+        city: String,
+        state: String,
+        country: String,
+        postalCode: String
+    },
+    created: {
+        type: Date,
+        default: Date.now()
+    }
+})
+
+userSchema.pre('save', (next) => {
+    const user = this
+
+    if (!user.isModified('password')) return next()
+
+    bcrypt.hash(user.password, null, null, (err, hash) => {
+        if (err) return next(err)
+
+        user.password = hash
+        next()
+    })
+})
+
+userSchema.methods.comparePassword = (password) => {
+    return bcrypt.compareSync(password, this.password)
+}
+
+userSchema.methods.gravatar = (size) => {
+    if (!this.size) size = 200
+    if (!this.email) return 'https://gravatar.com/avatar/?s' + size + '&d=retro'
+    const md5 = crypt.createHash('md5').update(this.email).digest('hex')
+
+    return 'https://gravatar.com/avatar/' + md5 + '?s' + size + '&d=retro'
+}
+
+module.exports = mongoose.model('User', userSchema)
