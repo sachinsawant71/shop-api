52d17e3e322dc4b906711a817a41c5355073149a
k1cho <aadnanhadzipasic@gmail.com>
2019-03-18 11:51:53 +0100

    seed database

--- routes/product.js
+++ routes/product.js
@@ -1,10 +1,27 @@
 const express = require('express')
 const router = express.Router()
+const faker = require('faker')
+const Product = require('../models/product')
 
 const Products = require('../controllers/products')
 const AuthMiddleware = require('../middlewares/auth')
 
 router.get('/seller', AuthMiddleware, Products.getSellerProducts)
 router.post('', AuthMiddleware, Products.post)
+
+router.get('/faker/test', (req, res, next) => {
+    for (let i = 0; i < 20; i++) {
+        const product = new Product()
+        product.category = '5c8f5b06ec212a08b6e9448e'
+        product.owner = '5c89388b536729669ad5b81f'
+        product.title = faker.commerce.productName()
+        product.description = faker.lorem.words()
+        product.price = faker.commerce.price()
+        product.save()
+    }
+    res.json({
+        message: 'Successfully added 20 products'
+    })
+})
 
 module.exports = router
